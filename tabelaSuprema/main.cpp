7ee68c5ccc37b5388c1bcbd08f8e9cb49b00a781
Bianca Rodrigues <binaksrc@gmail.com>
2018-09-13 12:22:31 -0300

    update tabelaSuperma (finished)

--- tabelaSuprema/main.cpp
+++ tabelaSuprema/main.cpp
@@ -52,21 +52,50 @@ void printHeader( const std::vector< std::string > & fields_) {
 }
 
 void printTable( const std::vector< std::string > & header_, const std::vector< std::string > & ints_) {
-    std::ostringstream oss_;
+    auto n_cols = header_.size();
+    
+    ofstream ofs_;
 
-    for (auto i(0u); i < ints_.size(); ++i) {
-        oss_ << "| ";
-        oss_ << setw(header_[i].size()) << setfill (' ') << ints_[i];
-        oss_ << " ";
-        
-        if (i % header_.size() == 0 && i != 0 ) {
-            oss_ << "|";
-            oss_ << '\n';
+    ofs_.open("f3", ios::app);
+
+    // O laco vai rodar a quantidade de linhas no arquivo de dados
+    for (auto i(0u); i < ints_.size(); ++i) { // Para cada linha de entrada (com mais de um numero)
+        std::istringstream iss_ (ints_[i]); // Prepara a linha para ser extraida
+        int data;
+   
+        // Esse laco extrai cada inteiro da mesma linha, um por vez 
+        for (auto ct_data(0u); ct_data < n_cols; ct_data++) {
+            iss_ >> data; // Extraindo numero da string para um inteiro
+            cout << "| ";
+            ofs_ << "| ";
+            cout << setw(header_[ct_data].size()) << setfill (' ') << data;
+            ofs_ << setw(header_[ct_data].size()) << setfill (' ') << data;
+            cout << " ";
+            ofs_ << " ";
         }
-    }
 
-    oss_ << endl;
-    cout << oss_.str();
+        cout << "|";
+        ofs_ << "|";
+        cout << endl;
+        ofs_ << endl;
+    }
+        
+    cout << "+";
+    ofs_ << "+";
+
+    for (auto i(0u); i < n_cols; ++i) {
+        auto n(header_[i].size());
+        cout << setw(n+2) << setfill('-') << "";
+        ofs_ << setw(n+2) << setfill('-') << "";
+        cout << "+";
+        ofs_ << "+";
+   }
+
+   cout << endl;
+   ofs_ << endl;
+
+   ofs_.close();
+
 }
 
 int main(int argc, char *argv[]) {
@@ -93,7 +122,7 @@ int main(int argc, char *argv[]) {
 
     ifs_.open(argv[2]);
 
-    while ( getline (ifs_, line, ' ') ) {
+    while ( getline (ifs_, line) ) {
         ints.push_back(line);
     }
     
